78c4d91a0bf44a61ef9e141b66391c7c7bb3720d
SHUBHAM9586 <109150297+SHUBHAM9586@users.noreply.github.com>
2024-02-20 17:11:47 +0530

    Add files via upload

---
+++ biggestnumber/src/biggestnumber/demo.java
@@ -0,0 +1,22 @@
+package biggestnumber;
+
+public class demo {
+
+	public static void main(String[] args) {
+		int a = 1;
+		int b = 40;
+		int c = 90;
+		
+		if (a>b && a>c) {
+			System.out.println(a);
+		}
+		else if (b>a && b>c) {
+			System.out.println(b);
+		}
+		else if (c>b && c>a) {
+			System.out.println(c);
+		}
+
+	}
+
+}
